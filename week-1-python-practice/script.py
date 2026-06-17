73370c98989b0bcaaaa3f4982dac1fa1ce1e61f0
Tnsae <tinsaemk6679@gmail.com>
2026-06-17 15:42:10 +0300

    improved file path handling

--- week-1-python-practice/script.py
+++ week-1-python-practice/script.py
@@ -8,12 +8,13 @@ def organize_files():
         if os.path.isdir(source_path):
             continue
         if f.endswith(".pdf"):
-            os.makedirs("downloads/pdfs",exist_ok=True)
-            shutil.move(source_path,"downloads/pdfs")
+            #use os.path.join("downloads","pdfs") instead of plain "downloads/pdfs"
+            os.makedirs(os.path.join("downloads","pdfs"),exist_ok=True)
+            shutil.move(source_path,os.path.join("downloads","pdfs",f))
         elif f.endswith(".md"):
-            os.makedirs("downloads/mds",exist_ok=True)
-            shutil.move(source_path,"downloads/mds")
+            os.makedirs(os.path.join("downloads","mds"),exist_ok=True)
+            shutil.move(source_path,os.path.join("downloads","mds",f))
         elif f.endswith(".txt"):
-            os.makedirs("downloads/txts",exist_ok=True)
-            shutil.move(source_path,"downloads/txts")
+            os.makedirs(os.path.join("downloads","txts"),exist_ok=True)
+            shutil.move(source_path,os.path.join("downloads","txts",f))
 organize_files()
