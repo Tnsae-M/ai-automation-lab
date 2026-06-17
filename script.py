7f990125bbca064a9d307afa7eae4366d20f184e
Tnsae <tinsaemk6679@gmail.com>
2026-06-17 15:20:33 +0300

    pyhon file to folder organizer script(improved)

--- script.py
+++ script.py
@@ -4,16 +4,16 @@ import shutil
 def organize_files():
     files=os.listdir("downloads")
     for f in files:
-        # source_path=os.path.join("download",f)
-        if os.path.isdir("downloads"+"/"+f):
+        source_path=os.path.join("downloads",f)
+        if os.path.isdir(source_path):
             continue
         if f.endswith(".pdf"):
             os.makedirs("downloads/pdfs",exist_ok=True)
-            shutil.move("downloads"+"/"+f,"downloads/pdfs")
-        if f.endswith(".md"):
+            shutil.move(source_path,"downloads/pdfs")
+        elif f.endswith(".md"):
             os.makedirs("downloads/mds",exist_ok=True)
-            shutil.move("downloads"+"/"+f,"downloads/mds")
-        if f.endswith(".txt"):
+            shutil.move(source_path,"downloads/mds")
+        elif f.endswith(".txt"):
             os.makedirs("downloads/txts",exist_ok=True)
-            shutil.move("downloads"+"/"+f,"downloads/txts")
+            shutil.move(source_path,"downloads/txts")
 organize_files()
