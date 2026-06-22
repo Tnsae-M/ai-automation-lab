86aaaeed135b23862e5419e4f03b37d1a1268c64
Tnsae <tinsaemk6679@gmail.com>
2026-06-22 13:08:01 +0300

    refined currency API

--- week-2-api-fundamentals/currency_converter.py
+++ week-2-api-fundamentals/currency_converter.py
@@ -9,14 +9,16 @@ def main():
     logger=setup_logger()
     from_curr=input("what is the currency u want to convert from: ").strip()
     to_curr=input("what is the currency u want to convert to: ").strip()
-    if not (from_curr or to_curr):
+    if not from_curr or not to_curr:
         logger.error("one currency is missing!")
+        sys.exit(1)
     amount=int(input("Amount to convert: "))
     if not amount:
         amount=1
     api= os.getenv("CURRENCY_API_KEY")
     if not api:
         logger.error("Api is missing")
+        sys.exit(1)
     change=get_currency(from_curr,to_curr,amount,api,logger)
     if change:
         display(change)
@@ -31,7 +33,6 @@ def setup_logger():
         logger.addHandler(handler)
     return logger
 def get_currency(curr_from,curr_to,amount,api_key,logger):
-    logger=setup_logger()
     url="http://api.currencylayer.com/convert"
     params={
         'access_key':api_key,
@@ -41,7 +42,7 @@ def get_currency(curr_from,curr_to,amount,api_key,logger):
     }
     try:
         res=requests.get(url,params=params)
-        res.raise_for_status
+        res.raise_for_status()
         return res.json()
     except requests.exceptions.RequestException as ex:
         logger.error("Error occured when fetching API: %s",ex)
@@ -49,6 +50,6 @@ def display(data):
     query=data["query"]
     result=data["result"]
     print("===CURRENCY change===")
-    print(f"{query["amount"]} {query["from"]} = {result} {query["to"]}")
+    print(f"{query['amount']} {query['from']} = {result} {query['to']}")
 
 main()
