961a43e988249cb3b8824106f825d1ecc0d007d4
Tnsae <tinsaemk6679@gmail.com>
2026-06-30 22:34:21 +0300

    built simple scheduler

--- week-3-bots-and-scheduling/telegram_weather_bot/scheduler.py
+++ week-3-bots-and-scheduling/telegram_weather_bot/scheduler.py
@@ -1,1 +1,9 @@
-import apscheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+import asyncio
+
+def say_hello():
+    print("Scheduler is working")
+scheduler=BlockingScheduler()
+scheduler.add_job(say_hello,trigger='interval',seconds=5)
+print("Starting scheduler...")
+scheduler.start()
