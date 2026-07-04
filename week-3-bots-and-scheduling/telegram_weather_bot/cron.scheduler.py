89853337a0661b701cb55fb36729c9ea66198b82
Tnsae <tinsaemk6679@gmail.com>
2026-07-05 00:48:45 +0300

    debugged cron job schedule and fixed no response issue. issue: Hour format(24 hr) misunderstanding

--- week-3-bots-and-scheduling/telegram_weather_bot/cron.scheduler.py
+++ week-3-bots-and-scheduling/telegram_weather_bot/cron.scheduler.py
@@ -1,7 +1,7 @@
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
-from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
+from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR,EVENT_JOB_MISSED
 from weather_service import get_weather
 from logger import setup_logger
 
@@ -9,6 +9,8 @@ logger=setup_logger()
 def listner(event):
     if event.exception:
         logger.error("The job crashed :(")
+    elif event.code == EVENT_JOB_MISSED:
+        logger.warning("The job was MISSED — it did not run in time!")
     else:
         logger.info("The job worked :)")
 def call_weather(city):
@@ -30,9 +32,10 @@ logger.info("Starting" \
 " the blocking scheduler...")
 
 scheduler2=BlockingScheduler()
-trigger=CronTrigger(hour=12,minute=32)
-job=scheduler2.add_job(call_weather,trigger=trigger,args=["Addis ababa"])
-scheduler2.add_listener(listner,EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
+# works in 24 hour format, so 0 is midnight and 12 is noon
+trigger=CronTrigger(hour=0,minute=45)
+job=scheduler2.add_job(call_weather,trigger=trigger,args=["Addis ababa"],misfire_grace_time=60)
+scheduler2.add_listener(listner,EVENT_JOB_EXECUTED | EVENT_JOB_ERROR|EVENT_JOB_MISSED)
 print(scheduler2.timezone)
 logger.info(f"Next fire is scheduled at {trigger.get_next_fire_time(None,datetime.datetime.now(scheduler2.timezone))}")
 scheduler2.start()
