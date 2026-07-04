c9040592fc9ec8e1e70323c06aeca0bba5d63d36
Tnsae <tinsaemk6679@gmail.com>
2026-07-05 00:31:44 +0300

    made progress in cron job scheduler

--- week-3-bots-and-scheduling/telegram_weather_bot/cron.scheduler.py
+++ week-3-bots-and-scheduling/telegram_weather_bot/cron.scheduler.py
@@ -1,10 +1,16 @@
+import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
+from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
 from weather_service import get_weather
 from logger import setup_logger
 
 logger=setup_logger()
-
+def listner(event):
+    if event.exception:
+        logger.error("The job crashed :(")
+    else:
+        logger.info("The job worked :)")
 def call_weather(city):
     weather=get_weather(city)
     if not weather:
@@ -19,11 +25,14 @@ def call_weather(city):
              f"country: {country}\n"
              f"temprature in Celcius: {temp_c}\n"
              f"condition: {condition} ")
-    print(message)
+    logger.info(message)
 logger.info("Starting" \
 " the blocking scheduler...")
 
 scheduler2=BlockingScheduler()
-scheduler2.add_job(call_weather,CronTrigger(hour=11,minute=35),args=["Addis ababa"])
+trigger=CronTrigger(hour=12,minute=32)
+job=scheduler2.add_job(call_weather,trigger=trigger,args=["Addis ababa"])
+scheduler2.add_listener(listner,EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
 print(scheduler2.timezone)
+logger.info(f"Next fire is scheduled at {trigger.get_next_fire_time(None,datetime.datetime.now(scheduler2.timezone))}")
 scheduler2.start()
