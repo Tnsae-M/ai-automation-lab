8aa2731ac16f54936268240ebab6b7e61c25d94b
Tnsae <tinsaemk6679@gmail.com>
2026-07-07 22:45:43 +0300

    wrote stateful gemini LLM response

--- week-4-AI-API-Integration/gemini_ai_model.py
+++ week-4-AI-API-Integration/gemini_ai_model.py
@@ -8,4 +8,11 @@ interaction = client.interactions.create(
     model="gemini-3.5-flash",
     input="you are a senior freelancer,Tell me how I can land an internation remote job that pays in dollars as an ethiopian backend developer using TS or JS as a language? consider that I have 2 years of experience in node.js developement and I have a portfolio of 5 projects that I can show to potential clients. Also, give me a list of 10 websites where I can find such jobs?"
 )
-print(interaction.output_text)
+print("Response 1: ",interaction.output_text)
+
+interaction_2= client.interactions.create(
+    model="gemini-3.5-flash",
+    input="from the previous answer, pick out the top 5 websites that I can focus on?",
+    previous_interaction_id=interaction.id
+)
+print("Response 2: ",interaction_2.output_text)
