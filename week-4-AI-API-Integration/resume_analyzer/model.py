cedc890a4216addcddb2510358fbf15c6674416b
Tnsae <tinsaemk6679@gmail.com>
2026-07-16 23:06:23 +0300

    feat(resume-analyser): added a pydantic model for resume

---
+++ week-4-AI-API-Integration/resume_analyzer/model.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel,Field
+from typing import Literal,List
+class Resume(BaseModel):
+    resume_score: int=Field(ge=0, le=10, description="A score representing the quality of the resume, ranging from 0 to 10.")
+    name: str=Field(description="The candidate's full name")
+    target_job:str=Field(description="The type of job the candidate is applying for")
+    languages: List[str]=Field(description="List of programming languages the candidate is proficient in.")
+    strength: List[str]=Field(description="The candidate's key strengths")
+    weakness: List[str]=Field(description="The candidate's key weaknesses")
+    suggestion: List[str]=Field(description="Suggestions for improvement.")
