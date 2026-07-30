f500e8b414560361a67d6952438084a02ee48d3b
Tnsae <tinsaemk6679@gmail.com>
2026-07-30 23:37:34 +0300

    feat(job-scraper): added single job details scraper

--- week-6-browser-automation-and-scraping/test_setup.py
+++ week-6-browser-automation-and-scraping/test_setup.py
@@ -29,6 +29,23 @@ with sync_playwright() as p:
                     "url":urljoin(page.url,href)
                 })
         print(f"Found {len(jobs)} jobs!")
-        pprint(jobs)
+        # pprint(jobs)
+
+        detail_page=browser.new_page()
+        first_job_url=jobs[1]["url"]
+
+        detail_page.goto(first_job_url,
+                 wait_until="domcontentloaded",
+                 timeout=40000)
+        detail_page.wait_for_load_state("networkidle")
+
+                # test for html structure.
+        paragraphs = detail_page.locator("p")
+        job_title=detail_page.locator("h1").nth(0).inner_text()
+
+        print(job_title)
+        for i in range([4,3,10,9,12,11,13]):
+            print(paragraphs.nth(i).inner_text())
+        
     finally:
         browser.close()
