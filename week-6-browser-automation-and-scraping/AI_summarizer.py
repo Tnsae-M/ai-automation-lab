b76032c077ea9f7e78d2014cbbf744f4105d3553
Tnsae <tinsaemk6679@gmail.com>
2026-08-06 00:07:35 +0300

    feat(job-alert): added retry handling logic for a failed request

--- week-6-browser-automation-and-scraping/AI_summarizer.py
+++ week-6-browser-automation-and-scraping/AI_summarizer.py
@@ -1,4 +1,6 @@
-from openai import OpenAI
+import time
+import json
+from openai import OpenAI,RateLimitError,APIConnectionError,APITimeoutError
 from job_data_model import JobSummary
 from config import openai_key
 client = OpenAI(
@@ -7,17 +9,26 @@ client = OpenAI(
 SYSTEM_PROMPT="""You are a senior Job Analyst. You are tasked with summarizing job postings into a structured format.
 You will be provided with job postings in JSON format. Your task is to extract the relevant information
 and return it in a structured format as specified below. If any information is missing, you can leave the field as null."""
-def summarize_job(job_text:str)->JobSummary:
-    try:
-        response=client.responses.parse(
-            model="gpt-5.4-mini",
-            input=[
-                {"role": "system", "content": SYSTEM_PROMPT},
-                {"role": "user", "content": f"Job posting data:\n\n{job_text}"}
-            ],
-            text_format=JobSummary
-        )
-        return response.output_parsed
-    except Exception as e:
-        print(f"Failed to summarize job posting: {e}")
-        return None
+def summarize_job(job_text:dict)->JobSummary|None:
+    max_attempt=5
+    job=json.dumps(job_text,indent=0)
+    for attempt in range(max_attempt):
+        try:
+            response=client.responses.parse(
+                model="gpt-5.4-mini",
+                input=[
+                    {"role": "system", "content": SYSTEM_PROMPT},
+                    {"role": "user", "content":job}
+                ],
+                text_format=JobSummary
+            )
+            return response.output_parsed
+        except (RateLimitError,APIConnectionError,APITimeoutError) as e:
+                if attempt !=max_attempt-1:
+                    time.sleep(2)
+                else:
+                    print(f"Job summarzing failed! due to {e}")
+                    return None
+        except Exception as e:
+            print(f"Failed to summarize job posting: {e}")
+            return None
--- week-6-browser-automation-and-scraping/main.py
+++ week-6-browser-automation-and-scraping/main.py
@@ -4,13 +4,14 @@ from AI_summarizer import summarize_job
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
-    try:
-        jobs=get_jobs(page)
-        for job in jobs:
+    jobs=get_jobs(page)
+    for job in jobs:
+        try:
             job_details=get_job_details(page,job["url"])
             job_summary=summarize_job(job_details)
             print(job_summary)
-        browser.close()
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        browser.close()
+        except Exception as e:
+            print(f"something went wrong on {job} job.")
+            continue
+    
+        
