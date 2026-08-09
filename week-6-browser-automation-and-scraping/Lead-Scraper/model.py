4b3ac72b751e5a27f291f5875bd861310dded1e1
Tnsae <tinsaemk6679@gmail.com>
2026-08-09 23:15:11 +0300

    feat(model): create leadModel for structured lead data representation

---
+++ week-6-browser-automation-and-scraping/Lead-Scraper/model.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel,Field
+from typing import List
+class leadModel(BaseModel):
+    name:str
+    address:str|None=None
+    phone:str|None=None
+    website:str|None=None
+    # enrichment fields
+    email:str|None=None
+    social_links:List[str]|None=None
+    about:str|None=Field(description="basic information about the buissness",default=None)
+
