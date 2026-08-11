75c6aefb0ffa0256aa9bb18aaad81a931f785c1a
Tnsae <tinsaemk6679@gmail.com>
2026-08-11 23:39:30 +0300

    fix(OSM-request-script): streamline area query syntax for Overpass API

--- week-6-browser-automation-and-scraping/Lead-Scraper/OSM-request-script.py
+++ week-6-browser-automation-and-scraping/Lead-Scraper/OSM-request-script.py
@@ -4,14 +4,10 @@ try:
     header={"User-Agent":"lead-scraper-learning-project/0.1 (https://github.com/Tnsae-M)"}
     url="https://overpass-api.de/api/interpreter"
     query=f"""[out:json];
-    area[name:en='{city}']->.searchArea;
+    area["name:en"='{city}'][boundary='administrative']->.searchArea;
     node['amenity'='cafe'](area.searchArea);
     out body;"""
-    query_test=f"""[out:json];
-    area["name:en"='{city}'][boundary='administrative']->.searchArea;
-    .searchArea out;
-"""
-    req=requests.post(url,data=query_test,headers=header,timeout=25)
+    req=requests.post(url,data=query,headers=header,timeout=25)
     print(req.json())
 except requests.exceptions.RequestException as e:
     print(f"error occured: {e}")
