0ec0d6ba54a8b2a95101b394462a51631158ab08
Tnsae <tinsaemk6679@gmail.com>
2026-08-21 00:45:36 +0300

    refactor: rename social_link_extractor to link_extractor for consistency

--- week-6-browser-automation-and-scraping/Lead-Scraper/social_media_link_extractor.py
+++ week-6-browser-automation-and-scraping/Lead-Scraper/social_media_link_extractor.py
@@ -1,8 +1,7 @@
 import re
 from playwright.sync_api import Page
-import json
 socials=["facebook","instagram","tiktok","twitter"]
-def social_link_extractor(page:Page)->set[str]:
+def link_extractor(page:Page)->set[str]:
     links:set[str]=set()
     hrefs:list[str]=page.eval_on_selector_all('a[href]','elements=>elements.map(el=>el.getAttribute("href"))')
     for href in hrefs:
